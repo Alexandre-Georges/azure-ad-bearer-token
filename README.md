0dca3e3f1deb2e922289955f91bb566d547f3e15
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2016-11-15 15:07:28 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # azure-ad-bearer-token
-Azure Active Directory with a bearer token
+
+This project takes care of the classic OAuth2.0 authentication via Azure Active Directory and also implements a token validation.
---
+++ views/authenticated.jsx
@@ -0,0 +1,41 @@
+var React = require('react');
+var DefaultLayout = require('./default');
+
+class AuthenticatedPage extends React.Component {
+    render () {
+        return (
+            <DefaultLayout>
+                <div>
+                    You are authenticated
+                    <br/>
+                    <br/>
+                    Your authentication code is {this.props.authenticationCode}
+                    <br/>
+                    <br/>
+                    Your access token is {this.props.accessToken}
+                    <br/>
+                    <br/>
+                    JWT header {this.props.jwt.header}
+                    <br/>
+                    JWT payload {this.props.jwt.payload}
+                    <br/>
+                    JWT signature {this.props.jwt.signature}
+                    <br/>
+                    <br/>
+                    <br/>
+                    <a href="/logout">Logout here</a>
+                    <br/>
+                    <br/>
+                    <form method="POST" action="/validateToken">
+                        Token validation <input type="text" name="token" value={this.props.accessToken} />&nbsp;<input type="submit" value="Validate" />
+                    </form>
+                    <br/>
+                    <br/>
+                    Validation result {this.props.validation}
+                </div>
+            </DefaultLayout>
+        );
+    }
+}
+
+module.exports = AuthenticatedPage;
